a98305dcdf3911da67313899fb896782b12c8913
허세진 <heosejin222@gmail.com>
2025-12-01 13:44:55 +0900

    init

---
+++ src/main/java/com/example/sejinboard/SejinboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.sejinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejinboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejinboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sejinboard/SejinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sejinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
